7c3bad90f651bc9765b96119b5f1d49df827b560
FireFlyForLife <maiko.s.99@gmail.com>
2019-11-05 21:36:20 +0100

    We have a abstract island generator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#![allow(dead_code)]
+
 //! A very simple shader example.
 
 use cgmath;
+use cgmath::{InnerSpace};
 use gfx::{self, *};
 use ggez;
 
@@ -8,8 +11,12 @@ use ggez::event;
 use ggez::graphics::{self, DrawMode, Vertex};
 use ggez::timer;
 use ggez::{Context, GameResult};
+
+use noise::{NoiseFn, Perlin, Seedable, utils::*};
+
 use std::env;
 use std::path;
+use std::vec::Vec;
 
 // Define the input struct for our shader.
 gfx_defines! {
@@ -39,9 +46,91 @@ fn make_quad(ctx: &mut Context, extends: cgmath::Point2::<f32>) -> GameResult<gr
     graphics::Mesh::from_raw(ctx, &my_vertices, &QUAD_INDICES, None)
 }
 
+type Point2I = cgmath::Vector2::<i32>;
+
+#[derive(Debug, Copy, Clone)]
+enum HexagonTile{
+    Land,
+    Water
+}
+
+#[derive(Default)]
+struct HexagonGrid{
+    hexagons: Vec<HexagonTile>,
+    width: usize,
+    height: usize
+}
+impl HexagonGrid{
+    fn new() -> Self{
+        let w = 100;
+        let h = 100;
+
+        let mut hexagons = Vec::with_capacity(w*h);
+
+        let perlin = Perlin::new().set_seed(4);
+        
+        // PlaneMapBuilder::new(&perlin)
+        // .set_x_bounds(0.0, 256.0)
+        // .set_y_bounds(0.0, 256.0)
+        // .set_size(512, 512)
+        // .build()
+        // .write_to_file("perlin.png");
+
+
+        for y in 0..h {
+            for x in 0..w {
+                let island = is_island(&perlin, cgmath::Vector2::<f32>{x: x as f32 + 0.5, y: y as f32 + 0.5}, w, h);
+
+                hexagons.push( if island { HexagonTile::Land } else { HexagonTile::Water});
+            }
+        } 
+
+        HexagonGrid{hexagons: hexagons, width: w, height: h}
+    }
+
+    fn print(&self) {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                print!("{:?}", self.hexagons[y*self.width+x] as u8);
+            }
+            println!("");
+        }
+    }
+}
+
+fn lerp(val: f64, from: f64, to: f64) -> f64 {
+    from + (to - from) * val
+}
+
+fn is_island(perlin: &Perlin, mut p: cgmath::Vector2::<f32>, w: usize, h: usize) -> bool {
+    // let c = perlin.get([ ((p.x+1.0)*128.0) as f64, ((p.y+1.0)*128.0) as f64]) ;
+    let mut c = perlin.get([ p.x as f64 * 128.5, p.y as f64 * 128.5]);
+    c = c * 0.5 + 0.5;
+    assert!(c >= 0.0 && c <= 1.0);
+
+
+    p.x /= w as f32;
+    p.y /= h as f32;
+
+    let island_center = cgmath::Vector2::<f32>::new(0.5, 0.5);
+    // p = p - island_center;
+
+    // let dist = p.x.abs().max(p.y.abs()) as f64;
+    // c = lerp(c, 0.5, 0.8);
+    // let island_center = cgmath::Vector2::<f32>::new(w as f32 * 0.5 , h as f32 * 0.5 );
+
+
+    c > (0.6 + 0.9 * (((p - island_center).magnitude() as f64)))
+    // c > 0.5
+    // c - (1.0 - 0.1) * dist * dist < 0.0
+}
+
+
 struct MainState {
     dim: Dim,
     shader: graphics::Shader<Dim>,
+
+    grid: HexagonGrid,
 }
 
 impl MainState {
@@ -55,7 +144,13 @@ impl MainState {
             "Dim",
             None,
         )?;
-        Ok(MainState { dim, shader })
+
+        let grid = HexagonGrid::new();
+        grid.print();
+
+        
+
+        Ok(MainState { dim, shader, grid })
     }
 }
 
